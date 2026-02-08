8397702f497f204fd3bbead6659b83549e42d62c
DV_r2d2 <99878145+t4hvz8@users.noreply.github.com>
2026-02-08 21:38:43 +0600

    ╨┐╤А╨╛╨▓╨╡╤А╨║╨░

--- push.bat
+++ push.bat
@@ -2,7 +2,7 @@
 # save as create_repo.sh
 git init
 git add .
-git commit -m "Initial commit"
+git commit -m "проверка"
 git branch -M main
 git remote add origin https://github.com/t4hvz8/brigadir.git
 git push -u origin main
