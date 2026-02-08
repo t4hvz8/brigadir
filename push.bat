17b172548aabc556feaf4abb73d6278e9ad3110d
DV_r2d2 <99878145+t4hvz8@users.noreply.github.com>
2026-02-08 21:41:16 +0600

    personal + tasks

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 #!/bin/bash
 git init
 git add .
-git commit -m "Персонал, задание реализованы"
+git commit -m "personal + tasks"
 git branch -M main
 git remote add origin https://github.com/t4hvz8/brigadir.git
 git push -u origin main
